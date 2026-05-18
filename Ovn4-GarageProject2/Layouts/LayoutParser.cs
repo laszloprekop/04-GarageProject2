e7a38681fb81725c693e4d856c39077e3f036e38
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-18 21:39:38 +0200

    prep(layouts): add a primitive LayoutParser
    Converts a blueprint to the in-memory, iterable data model

---
+++ Ovn4-GarageProject2/Layouts/LayoutParser.cs
@@ -0,0 +1,29 @@
+namespace Ovn4_GarageProject2.Layouts;
+using Domain;
+
+public static class LayoutParser
+{
+    private static readonly HashSet<char> RoadChars =
+        [' ', '│', '─', '├', '┤', '┼', '┬', '┴'];
+
+    public static Garage<T> Parse<T>(string name, string[] blueprint) where T : Vehicle
+    {
+        int rows = blueprint.Length;
+        int cols = blueprint.Max(r => r.Length);
+        var grid = new GarageCell[rows, cols];
+
+        for (int r = 0; r < rows; r++)
+        for (int c = 0; c < cols; c++)
+        {
+            char ch = c < blueprint[r].Length ? blueprint[r][c] : ' ';
+            grid[r, c] = ch switch
+            {
+                '░'                    => new WallCell(),
+                _ when RoadChars.Contains(ch) => new RoadCell(),
+                _                      => new ParkingSpot { Id = r * cols + c, Row = r, Col = c },
+            };
+        }
+
+        return new Garage<T>(name, grid);
+    }
+}
